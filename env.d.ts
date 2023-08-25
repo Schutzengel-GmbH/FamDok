2e0012321e15a13ac9d0aa82fb413fd56e890165
Carsten Menge <cmenge@schutzengel-flensburg.de>
2023-08-25 17:13:16 +0200

    fix wrong port in example env

--- env.d.ts
+++ env.d.ts
@@ -1,123 +1,144 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    /**
+/**
+     * @example
+     * ```
+    process.env.ADMIN_EMAIL
+    ```
+     */
+    ADMIN_EMAIL: string
+/**
+     * @example
+     * ```
+    process.env.ADMIN_PASSWORD
+    ```
+     */
+    ADMIN_PASSWORD: string
+/**
      * @example
      * ```
     process.env.POSTGRES_USER
     ```
      */
-    POSTGRES_USER: string;
-    /**
+    POSTGRES_USER: string
+/**
      * @example
      * ```
     process.env.POSTGRES_PASSWORD
     ```
      */
-    POSTGRES_PASSWORD: string;
-    /**
+    POSTGRES_PASSWORD: string
+/**
      * @example
      * ```
     process.env.POSTGRES_DB
     ```
      */
-    POSTGRES_DB: string;
-    /**
+    POSTGRES_DB: string
+/**
      * @example
      * ```
     process.env.POSTGRES_APP_USER
     ```
      */
-    POSTGRES_APP_USER: string;
-    /**
+    POSTGRES_APP_USER: string
+/**
      * @example
      * ```
     process.env.POSTGRES_APP_PASSWORD
     ```
      */
-    POSTGRES_APP_PASSWORD: string;
-    /**
+    POSTGRES_APP_PASSWORD: string
+/**
      * @example
      * ```
     process.env.POSTGRES_APP_DB
     ```
      */
-    POSTGRES_APP_DB: string;
-    /**
+    POSTGRES_APP_DB: string
+/**
      * @example
      * ```
     process.env.POSTGRES_HOST
     ```
      */
-    POSTGRES_HOST: string;
-    /**
+    POSTGRES_HOST: string
+/**
      * @example
      * ```
     process.env.SUPERTOKENS_CONNECTION_URI
     ```
      */
-    SUPERTOKENS_CONNECTION_URI: string;
-    /**
+    SUPERTOKENS_CONNECTION_URI: string
+/**
      * @example
      * ```
-    process.env.APP_URL
+    process.env.NEXT_PUBLIC_APP_URL
     ```
      */
-    NEXT_PUBLIC_APP_URL: string;
-    /**
+    NEXT_PUBLIC_APP_URL: string
+/**
+     * @example
+     * ```
+    process.env.NEXT_PUBLIC_APP_NAME
+    ```
+     */
+    NEXT_PUBLIC_APP_NAME: string
+/**
      * @example
      * ```
     process.env.API_KEY
     ```
      */
-    API_KEY: string;
-    /**
+    API_KEY: string
+/**
      * @example
      * ```
     process.env.DATABASE_URL
     ```
      */
-    DATABASE_URL: string;
-    /**
+    DATABASE_URL: string
+/**
      * @example
      * ```
     process.env.SMTP_HOST
     ```
      */
-    SMTP_HOST: string;
-    /**
+    SMTP_HOST: string
+/**
      * @example
      * ```
     process.env.SMTP_AUTH_NAME
     ```
      */
-    SMTP_AUTH_NAME: string;
-    /**
+    SMTP_AUTH_NAME: string
+/**
      * @example
      * ```
     process.env.SMTP_PASSWORD
     ```
      */
-    SMTP_PASSWORD: string;
-    /**
+    SMTP_PASSWORD: string
+/**
      * @example
      * ```
     process.env.SMTP_PORT
     ```
      */
-    SMTP_PORT: string;
-    /**
+    SMTP_PORT: string
+/**
      * @example
      * ```
     process.env.SMTP_FROM_NAME
     ```
      */
-    SMTP_FROM_NAME: string;
-    /**
+    SMTP_FROM_NAME: string
+/**
      * @example
      * ```
     process.env.SMTP_FROM_EMAIL
     ```
      */
-    SMTP_FROM_EMAIL: string;
+    SMTP_FROM_EMAIL: string
   }
 }
