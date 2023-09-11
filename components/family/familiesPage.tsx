38ecdaf7f47a1bdf68c7b574813a2f7d83a5fc43
Carsten Menge <cmenge@schutzengel-flensburg.de>
2023-09-11 13:06:35 +0200

    add new family in family view #10

--- components/family/familiesPage.tsx
+++ components/family/familiesPage.tsx
@@ -1,7 +1,11 @@
 import { FamilyCard } from "@/components/family/familyCard";
+import FamilyDialog from "@/components/family/familyDialog";
 import SearchTextField from "@/components/utilityComponents/searchTextField";
 import { FullFamily } from "@/types/prismaHelperTypes";
-import { Box, TextField } from "@mui/material";
+import { Add } from "@mui/icons-material";
+import { Box, Button, TextField } from "@mui/material";
+import { Gender, Education, Disability } from "@prisma/client";
+import { GetResult } from "@prisma/client/runtime";
 import { useState } from "react";
 
 type FamiliesPageProps = {
@@ -14,23 +18,39 @@ export function FamiliesPageComponent({
   onChange,
 }: FamiliesPageProps) {
   const [filter, setFilter] = useState("");
+  const [newFamOpen, setNewFamOpen] = useState(false);
 
   function fncFilter(family: FullFamily) {
     if (!filter) return true;
     return filter.toLowerCase().includes(family.number.toString());
   }
 
+  function handleNew() {
+    setNewFamOpen(true);
+  }
+
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", gap: ".5rem" }}>
-      <SearchTextField
-        label="Filter: Familiennummer"
-        filter={filter}
-        onChange={setFilter}
+    <>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: ".5rem" }}>
+        <SearchTextField
+          label="Filter: Familiennummer"
+          filter={filter}
+          onChange={setFilter}
+        />
+        <Button onClick={handleNew}>
+          <Add />
+          Neue Familie
+        </Button>
+        {families?.filter(fncFilter).map((f) => (
+          <FamilyCard key={f.id} family={f} onChange={onChange} />
+        ))}
+      </Box>
+      <FamilyDialog
+        initialFamily={undefined}
+        open={newFamOpen}
+        onClose={() => setNewFamOpen(false)}
       />
-      {families?.filter(fncFilter).map((f) => (
-        <FamilyCard key={f.id} family={f} onChange={onChange} />
-      ))}
-    </Box>
+    </>
   );
 }
 
