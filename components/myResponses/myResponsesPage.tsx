46351b971ad101642ea6b9e45aba6eda18354076
Carsten Menge <cmenge@schutzengel-flensburg.de>
2023-12-08 16:53:58 +0100

    extended Filters

--- components/myResponses/myResponsesPage.tsx
+++ components/myResponses/myResponsesPage.tsx
@@ -1,16 +1,11 @@
+import { FilterType, ResponseFilter } from "@/components/myResponses/filter.t";
+import { filterByQuestionAnswer } from "@/components/myResponses/logic";
 import ResponseCard from "@/components/myResponses/responseCard";
-import ResponsesFilter, {
-  ResponseFilter,
-} from "@/components/myResponses/responsesFilter";
+import ResponseFilterComponent from "@/components/myResponses/responseFilterComponent";
+
 import ErrorPage from "@/components/utilityComponents/error";
-import {
-  FullAnswer,
-  FullResponse,
-  PartialAnswer,
-} from "@/types/prismaHelperTypes";
 import { useMyResponses, useSurvey } from "@/utils/apiHooks";
 import { Box, CircularProgress } from "@mui/material";
-import { isSameDay, parse, parseISO } from "date-fns";
 import { useState } from "react";
 
 type MyResponsesPageProps = {
@@ -18,27 +13,34 @@ type MyResponsesPageProps = {
 };
 
 export default function MyResponsesPageComponent({ id }: MyResponsesPageProps) {
-  const [answerFilter, setAnswerFilter] = useState<ResponseFilter>(undefined);
   const { error, isLoading, isValidating, mutate, responses } =
     useMyResponses(id);
+  const [responsesFilter, setResponsesFilter] =
+    useState<ResponseFilter<FilterType>>(undefined);
 
   const { survey } = useSurvey(id);
 
   if (isLoading) return <CircularProgress />;
   if (error) return <ErrorPage message={error} />;
 
+  function handleChangeFilter(f: ResponseFilter<FilterType>) {
+    setResponsesFilter(f);
+  }
+
+  console.log(responsesFilter);
+
   return (
     <Box>
-      <ResponsesFilter
+      <ResponseFilterComponent
+        filter={responsesFilter}
+        onChange={handleChangeFilter}
         survey={survey}
-        answerFilter={answerFilter}
-        onChange={setAnswerFilter}
       />
       {responses
-        ?.filter(filterByQuestionAnswer(answerFilter))
+        ?.filter(filterByQuestionAnswer(responsesFilter))
         .filter((r) =>
-          answerFilter?.filterFamily
-            ? r.familyId === answerFilter.familyId
+          responsesFilter?.filterFamily
+            ? r.familyId === responsesFilter.familyId
             : true
         )
         .map((r) => (
@@ -46,45 +48,5 @@ export default function MyResponsesPageComponent({ id }: MyResponsesPageProps) {
         ))}
     </Box>
   );
-}
-
-function filterByQuestionAnswer(filter: ResponseFilter) {
-  if (!filter || !filter.filter) return () => true;
-
-  return (response: FullResponse) => {
-    const answer = response.answers.find(
-      (q) => q.questionId === filter.question?.id
-    );
-
-    switch (filter.question?.type) {
-      case "Text":
-        return answer?.answerText === filter.filter.answerText;
-      case "Bool":
-        return answer?.answerBool === filter.filter.answerBool;
-      case "Int":
-        return answer?.answerInt === filter.filter.answerInt;
-      case "Num":
-        return answer?.answerNum === filter.filter.answerNum;
-      case "Date":
-        return isSameDay(
-          new Date(answer?.answerDate),
-          new Date(filter.filter.answerDate)
-        );
-      case "Scale":
-      case "Select":
-        return areSameSelectAnswers(answer, filter.filter);
-    }
-  };
-}
-
-function areSameSelectAnswers(a: PartialAnswer, b: PartialAnswer) {
-  if (a?.answerSelect == undefined || b?.answerSelect == undefined)
-    return false;
-
-  if (a.answerSelect.length !== b.answerSelect.length) return false;
-
-  return a.answerSelect.reduce((prev, oa) => {
-    return b.answerSelect.find((ob) => ob.id === oa.id) ? prev && true : false;
-  }, true);
 }
 
