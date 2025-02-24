b187608b0cb7d73d4b040ddf0d239eb7b35d0ae3
Carsten Menge <cmenge@schutzengel-flensburg.de>
2025-02-24 15:01:24 +0100

    migrate via prisma script

---
+++ prisma/migrations/20250221122321_/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "Survey" DROP CONSTRAINT "Survey_masterDataTypeId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Survey" ADD CONSTRAINT "Survey_masterDataTypeId_fkey" FOREIGN KEY ("masterDataTypeId") REFERENCES "MasterDataType"("id") ON DELETE SET NULL ON UPDATE CASCADE;
