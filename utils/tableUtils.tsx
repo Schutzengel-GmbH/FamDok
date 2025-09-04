355ce909fe52164a8b81e4a924fc7057c3f4b76a
Carsten Menge <cmenge@schutzengel-flensburg.de>
2025-09-04 12:52:47 +0200

    don't show triggersurvey

--- utils/tableUtils.tsx
+++ utils/tableUtils.tsx
@@ -738,6 +738,8 @@ export function masterDataColumnDefinitions(
                   formatter: collectionFormatter,
                   formatterParams: { collectionType: dataField.collectionType },
                 };
+              case "TriggerSurvey":
+                return {title: "", visible: false}
               default:
                 return { title: "--FEHLER--" };
             }
